f828c9cfd01ef117a7ea0995e23624dea530474e
damienbod <damien_bod@hotmail.com>
2014-06-19 11:36:59 +0200

    adding OData V4 CRUD for AnimalType controller

--- WebAPIODataV4SQLite/Controllers/AnimalTypeController.cs
+++ WebAPIODataV4SQLite/Controllers/AnimalTypeController.cs
@@ -1,6 +1,10 @@
-﻿using System.Linq;
+﻿using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.OData;
+using System.Web.OData.Routing;
 using WebAPIODataV4SQLite.DomainModel;
 
 namespace WebAPIODataV4SQLite.Controllers
@@ -25,6 +29,85 @@ namespace WebAPIODataV4SQLite.Controllers
         {
             return Ok(_sqliteContext.AnimalTypeEntities.Find(key));
         }
+
+        [HttpPost]
+        [ODataRoute("AnimalType")]
+        public async Task<IHttpActionResult> CreateEventData(AnimalType animalType)
+        {
+            if (animalType != null && !ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _sqliteContext.AnimalTypeEntities.Add(animalType);
+            await _sqliteContext.SaveChangesAsync();
+
+            return Created(animalType);
+        }
+
+        [HttpPut]
+        [ODataRoute("AnimalType")]
+        public async Task<IHttpActionResult> Put([FromODataUri] int key, AnimalType animalType)
+        {
+            if (animalType != null && !ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (!_sqliteContext.AnimalTypeEntities.Any(t => t.Id == animalType.Id && t.Id == key))
+            {
+                return Content(HttpStatusCode.NotFound, "NotFound");
+            }
+
+            _sqliteContext.AnimalTypeEntities.AddOrUpdate(animalType);
+            await _sqliteContext.SaveChangesAsync();
+
+            return Updated(animalType);
+        }
+
+        [HttpPut]
+        [ODataRoute("AnimalType")]
+        public async Task<IHttpActionResult> Patch([FromODataUri] int key, Delta<AnimalType> delta)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (!_sqliteContext.AnimalTypeEntities.Any(t => t.Id == key))
+            {
+                return Content(HttpStatusCode.NotFound, "NotFound");
+            }
+
+            var animalType = _sqliteContext.AnimalTypeEntities.Single(t => t.Id == key);
+            delta.Patch(animalType);
+            await _sqliteContext.SaveChangesAsync();
+
+            return Updated(animalType);
+        }
+
+        [HttpDelete]
+        [ODataRoute("AnimalType")]
+        public async Task<IHttpActionResult> Delete([FromODataUri] int key)
+        {
+            var entity = _sqliteContext.AnimalTypeEntities.FirstOrDefault(t => t.Id == key);
+
+            if (entity == null)
+            {
+                return Content(HttpStatusCode.NotFound, "NotFound");
+            }
+
+            if (entity.EventDataValues.Any())
+            {
+                return BadRequest("The entity has child EventData entites still, remove these first");
+            }
+
+            _sqliteContext.AnimalTypeEntities.Remove(entity);
+            await _sqliteContext.SaveChangesAsync();
+
+            return Content(HttpStatusCode.NoContent, "Deleted");
+        }
+
 
         protected override void Dispose(bool disposing)
         {
